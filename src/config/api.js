16c8af9f0c1bb49b4be866beebb01a5ca30221ee
Mikael Morshinin <mvm@spnv.io>
2025-11-20 21:03:52 +0400

    feat/glasses-to-db: add glasses to db

--- src/config/api.js
+++ src/config/api.js
@@ -3,5 +3,6 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const RECIPES_URL = `${API_URL}/recipes`;
 export const COMPONENTS_URL = `${API_URL}/components`;
 export const METHODS_URL = `${API_URL}/methods`;
+export const GLASSES_URL = `${API_URL}/glasses`;
 export const UPLOAD_URL = `${API_URL}/upload`;
 export const INSTRUCTIONS_URL = `${API_URL}/instructions`;
