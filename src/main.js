87da5fa59a51a5a69318a8de969ccc8241396b6a
Mikael Morshinin <mvm@spnv.io>
2025-11-30 21:45:46 +0400

    feat: add internationalization (i18n) support with English and Russian locales and update router titles to use translation keys.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import { createPinia } from "pinia";
+import i18n from './i18n';
 import './style.css'
 
 import Antd from 'ant-design-vue'
@@ -12,5 +13,6 @@ const pinia = createPinia();
 createApp(App)
   .use(router)
   .use(pinia)
+  .use(i18n)
   .use(Antd)
   .mount('#app')
