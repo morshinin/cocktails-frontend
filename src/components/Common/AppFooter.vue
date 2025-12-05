b2af9957cd5833aac64c8086455664e0f5b01317
Mikael Morshinin <mvm@spnv.io>
2025-12-05 18:34:26 +0400

    refactor: extract footer component from App.vue

---
+++ src/components/Common/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    <div class="flex flex-col items-center gap-4">
+      <div class="flex gap-8 text-gray-500">
+        <span>© 2024 Cocktails App</span>
+        <span>v1.2.0</span>
+      </div>
+      
+      <div class="flex gap-8">
+        <a href="#" class="text-gray-500 hover:text-blue-500">Помощь / Инструкции</a>
+        <a href="#" class="text-gray-500 hover:text-blue-500">Сообщить об ошибке</a>
+        <a href="#" class="text-gray-500 hover:text-blue-500">Политика конфиденциальности</a>
+      </div>
+    </div>
+  </a-layout-footer>
+</template>
